d1d46507095d3e8514e55a4a0aba8afe8fd6922a
RyanB28 <45873654+RyanB28@users.noreply.github.com>
2020-06-03 22:16:02 +0200

    WebHooks

--- WebHooks/views.py
+++ WebHooks/views.py
@@ -6,15 +6,26 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 
 
 IntentFulfilmentText = {
-    "DjangoTest" : ["Dit is vanuit 1", "This is a test"], 
-    "DjangoTest2" : ["Dit is vanuit 2", "This is a test"],
+    "Is_er_vandaag_iets_bijzonders" : ["Dit is vanuit 1", "This is a test"], 
+    "Wat_moet_er_voor_morgen_gebeuren" : ["Dit is vanuit 2", "This is a test"],
+    "Wat zijn de laatste updates" : ["Dit is vanuit 3"],
+    "Wie_is_er_vandaag_jarig" : ["Dit is vanuit 4"],
+    "" : "Something went wrong, try again later."
     }
 
 # Create your views here.
 @csrf_exempt
 def TestRepsonse(request):
     req = json.loads(request.body)
-    IntentName = req["queryResult"]["intent"]["displayName"]
-    FulfilmentText = IntentFulfilmentText[IntentName][0]
-    print(IntentName, FulfilmentText)
-    return JsonResponse({"fulfillmentText": FulfilmentText}, safe=False)
+    IntentName = None
+    FulfilmentText = None
+    try:
+        IntentName = req["queryResult"]["intent"]["displayName"]
+        FulfilmentText = IntentFulfilmentText[IntentName][0]
+        print(IntentName, FulfilmentText)
+    except:
+        IntentName = ""
+        FulfilmentText = "Something went wrong, please try again."
+    finally:
+        print(IntentName, FulfilmentText)
+        return JsonResponse({"fulfillmentText": FulfilmentText}, safe=False)
